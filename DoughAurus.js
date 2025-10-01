f891a01aa3064c323ef5502feb07ab587c10957c
Panagiotis Papadopoulos <126814800+PanaPapad@users.noreply.github.com>
2025-10-01 16:46:18 +0300

    Add automatic clicking for golden cookies and reindeers
    This script automatically clicks golden cookies and reindeers in the game Cookie Clicker, while avoiding wrath cookies.

---
+++ DoughAurus.js
@@ -0,0 +1,42 @@
+// Golden Cookie Clicker
+// By RainSlide
+// Automatically click golden cookies and reindeers, won't click wrath cookies.
+// Version 1.0
+// Updated 2022-04-19
+
+// Cookie Clicker: https://orteil.dashnet.org/cookieclicker/
+
+"use strict";
+
+if (typeof Game !== "object" || Game === null || !Array.isArray(Game.shimmers)) {
+
+	console.error(
+		"Game is not ready yet. `Good things come to those who wait`"
+	);
+
+}else {
+
+	const apply = (target, _this, args) => {
+		var shimmer = args[0];
+		if ((shimmer.type === "golden" && !shimmer.wrath && !isCookieStormActive()) || shimmer.type === "reindeer") {
+			setTimeout(() => shimmer.pop(), 500);
+		}
+		return Reflect.apply(target, _this, args);
+	};
+
+	Object.defineProperty(Game.shimmers, "push", {
+		value: new Proxy(Game.shimmers.push, { apply }),
+		writable: true,
+		enumerable: false,
+		configurable: true
+	});
+
+	typeof Game.Win === "function" &&
+	Game.Win("Third-party");
+
+
+}
+
+function isCookieStormActive(){
+  return Object.keys(Game.buffs).some(b => b.toLowerCase().includes("cookie storm"));
+}
